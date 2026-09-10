f0132c02ad03c91012cc075ef739b3e700f6b8b8
Souzalivss <souzalivia8412@gmail.com>
2026-09-10 09:18:39 -0300

    tabuada

---
+++ exer67.py
@@ -0,0 +1,7 @@
+while True:
+    n = int(input('Digite um numero: '))
+    if n < 0:
+        break
+    for c in range(1, 11):
+        print(f'{n} x {c} = {n * c}')
+print('PROGRAMA TABUADA ENCERRADO')
