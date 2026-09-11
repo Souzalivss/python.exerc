c0763e55af289a322f662e9716fa0b021be6ec0e
Souzalivss <souzalivia8412@gmail.com>
2026-09-11 08:07:54 -0300

    par.ou.impar

--- exer67.py
+++ exer67.py
@@ -1,7 +1,26 @@
+from random import randint
+v = 0
 while True:
-    n = int(input('Digite um numero: '))
-    if n < 0:
-        break
-    for c in range(1, 11):
-        print(f'{n} x {c} = {n * c}')
-print('PROGRAMA TABUADA ENCERRADO')
+    jogador = int(input('jogue um valor: '))
+    computador = randint(0, 11)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('par ou impar? [P/I] ')).strip().upper()[0]
+    print(f'Voce jogou {jogador} e o computador {computador}. Total de {total}')
+    if tipo == 'P':
+            if total % 2 == 0:
+                print('Voce ganhou')
+                v += 1
+            else:
+                print('Voce perdeu')
+                break
+    elif tipo == 'I':
+            if total % 2 == 1:
+                print('Voce ganhou')
+                v += 1
+            else:
+                print('Voce perdeu')
+                break
+            print('Vamos jogar novamente...')
+print(f'GAME OVER! voce venceu {v} vezes')
