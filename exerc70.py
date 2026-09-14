36797da8bc1afdfaf773bba57bfd9be5dbbb940c
Souzalivss <souzalivia8412@gmail.com>
2026-09-14 08:33:52 -0300

    estatística.produtos.barato

--- exerc70.py
+++ exerc70.py
@@ -1,4 +1,5 @@
 total = totmil = cont = menor = 0
+barato = ''
 while True:
     produto = str(input('nome do produto: '))
     preço = float(input('Preço = R$'))
@@ -8,9 +9,11 @@ while True:
         totmil += 1
     if cont == 1:
         menor = preço
+        barato = produto
     else:
         if preço < menor:
               menor = preço
+              barato = produto
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
@@ -19,4 +22,4 @@ while True:
 print('{:-^40}'.format('FIM DO PROGRAMA'))
 print(f'o total da compra foi R${total:.2f}')
 print(f'A quantidade de produtos acima de R$1000,0 é {totmil}')
-print(f'o produto mais barato custa R${menor:.2f}')
+print(f'o produto mais barato foi {barato} e custa R${menor:.2f}')
