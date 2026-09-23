f7ff65d1e39bb11f04815d50f86169d5526ab5cd
Souzalivss <souzalivia8412@gmail.com>
2026-09-23 08:57:18 -0300

    valoresUnicosEmUmaLista

---
+++ exerc79.py
@@ -0,0 +1,12 @@
+numeros = list()
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    r = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if r in 'Nn':
+        break
+print('Voce digitou os valores {}'.format(numeros))
