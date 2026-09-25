9cb2db9adc8c2a69795873dd0ef5f21e2266e1c2
Souzalivss <souzalivia8412@gmail.com>
2026-09-25 08:37:20 -0300

    expressão.válidaOUinválida

---
+++ exerc83.py
@@ -0,0 +1,15 @@
+expr = str(input('Digite sua expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop() #remove o ultimo elemento de uma lista
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
